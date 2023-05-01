7007d9443ad0ef7861c03588944003c8e7190769
Syed-Faheemuddin <63056474+Syed-Faheemuddin@users.noreply.github.com>
2023-05-01 13:49:02 +0530

    added files

---
+++ cc_a2/assign.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request
+import json
+from json.decoder import JSONDecodeError
+import random
+app = Flask(__name__)
+
+class UserController:
+    def __init__(self,surname, name):
+        self.id = random.randint(1000,9999)
+        self.surname = surname
+        self.name = name
+    
+    def get_id(self):
+        return self.id
+    def get_surname(self):
+        return self.surname
+    def get_name(self):
+        return self.name
+    #def get_date(self):
+        #return self.datecreation
+
+def load_records(records):
+    with open("records.json", 'r+') as file:
+        try:
+            file_data = json.load(file)
+        except json.decoder.JSONDecodeError:
+            file_data = {"details": []}
+        file_data["details"].append(records)
+        file.seek(0)
+        json.dump(file_data, file, indent=4)
+
+@app.route('/', methods = ['GET'])
+def index():
+    return render_template('menu.html')
+
+@app.route('/index21.html', methods=['GET'])
+def button1():
+    return render_template('index21.html')
+
+@app.route('/createrecord', methods = ['POST'])
+def createRecord():
+    surname = request.form['Surname']
+    name = request.form['name']
+    u1 = UserController(surname, name)
+    records = {"id": u1.get_id(),"surname": u1.get_surname(), "name": u1.get_name()}
+    data = load_records(records)
+    print(data)
+    return f"ID: <b>{u1.get_id()}</b><br>Name: <b>{u1.get_name()}</b><br>Surname: <b>{u1.get_surname()}</b>"
+
+@app.route('/records.html', methods = ['GET'])
+def display_records():
+    with open("records.json", "r") as f:
+        data = json.load(f)
+    return render_template('records.html', records=data['details'], colnames=['id', 'surname', 'name'])
+
+@app.route("/recordid.html", methods = ['GET'])
+def button2():
+    return render_template('recordid.html')
+
+@app.route("/recordid.html", methods = ['POST'])
+def getRecord():
+    id = request.form['id']
+    with open("records.json", "r") as f:
+        data = json.load(f)
+        for item in data['details']:
+            if int(id) == int(item['id']):		
+            	return f"Name: <b>{str(item['name'])}</b><br>Surname: <b>{str(item['surname'])}</b><br><br>"
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 8000)
