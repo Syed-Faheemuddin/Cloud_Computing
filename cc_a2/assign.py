59653495170f6bd2281ca70738d591e8638458c7
Syed-Faheemuddin <63056474+Syed-Faheemuddin@users.noreply.github.com>
2023-05-02 12:25:19 +0530

    configured the server to be accessed publicly

--- cc_a2/assign.py
+++ cc_a2/assign.py
@@ -67,4 +67,4 @@ def getRecord():
             	return f"Name: <b>{str(item['name'])}</b><br>Surname: <b>{str(item['surname'])}</b><br><br>"
 
 if __name__ == "__main__":
-    app.run(debug = True, port = 8000)
+    app.run(host='0.0.0.0',debug = True, port = 8000)
