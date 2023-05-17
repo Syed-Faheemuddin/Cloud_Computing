033af6fe4909b961b6fbc1705d164c9a444fbe5b
Syed-Faheemuddin <63056474+Syed-Faheemuddin@users.noreply.github.com>
2023-05-17 19:41:57 +0530

    Updated to show timestamp

--- cc_a2/assign.py
+++ cc_a2/assign.py
@@ -2,22 +2,24 @@ from flask import Flask, render_template, request
 import json
 from json.decoder import JSONDecodeError
 import random
+from datetime import datetime
+
 app = Flask(__name__)
 
 class UserController:
-    def __init__(self,surname, name):
+    def __init__(self,surname, name,timestamp):
         self.id = random.randint(1000,9999)
         self.surname = surname
         self.name = name
-    
+        self.timestamp=timestamp    
     def get_id(self):
         return self.id
     def get_surname(self):
         return self.surname
     def get_name(self):
         return self.name
-    #def get_date(self):
-        #return self.datecreation
+    def get_timestamp(self):
+        return self.timestamp
 
 def load_records(records):
     with open("records.json", 'r+') as file:
@@ -41,11 +43,12 @@ def button1():
 def createRecord():
     surname = request.form['Surname']
     name = request.form['name']
-    u1 = UserController(surname, name)
-    records = {"id": u1.get_id(),"surname": u1.get_surname(), "name": u1.get_name()}
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    u1 = UserController(surname, name,timestamp)
+    records = {"id": u1.get_id(),"surname": u1.get_surname(), "name": u1.get_name(),"timestamp":u1.get_timestamp()}
     data = load_records(records)
     print(data)
-    return f"ID: <b>{u1.get_id()}</b><br>Name: <b>{u1.get_name()}</b><br>Surname: <b>{u1.get_surname()}</b>"
+    return f"ID: <b>{u1.get_id()}</b><br>Name: <b>{u1.get_name()}</b><br>Surname: <b>{u1.get_surname()}</b><br>Timestamp: <b>{u1.get_timestamp()}</b>"
 
 @app.route('/records.html', methods = ['GET'])
 def display_records():
@@ -64,7 +67,7 @@ def getRecord():
         data = json.load(f)
         for item in data['details']:
             if int(id) == int(item['id']):		
-            	return f"Name: <b>{str(item['name'])}</b><br>Surname: <b>{str(item['surname'])}</b><br><br>"
+            	return f"Name: <b>{str(item['name'])}</b><br>Surname: <b>{str(item['surname'])}</b><br><br><br>Timestamp: <b>{u1.get_timestamp()}</b>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug = True, port = 8000)
+    app.run(debug = True, host='0.0.0.0',port = 8000)
